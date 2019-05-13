d9a43e7e6c2e26ad61ebbdd3af6435ba7c05978c
martijn <martijn.vandebrug@incentro.com>
2019-05-13 18:13:15 +0200

    moved images to 1 list, with tags to include in a category

--- web-dist/js/portfolio.js
+++ web-dist/js/portfolio.js
@@ -1,12 +1,20 @@
 function showImages(name) {
-    $('#freehand-images').hide();
-    $('#stencil-images').hide();
-    $('#workshop-images').hide();
-    $('#shop-images').hide();
-
-   $(name).show();
+    $('.freehand').hide();
+    $('.stencil').hide();
+    $('.workshop').hide();
+    $('.shop').hide();
+    $(name).show();
 }
 
 function hideImage() {
     $('#image-overlay').hide();
 }
+
+function showImage(imageUrl) {
+    $('#image-overlay').html(`<img class="img-fluid centerImage"src="${imageUrl}">`);
+    $('#image-overlay').show();
+}
+
+$(document).ready(function () {
+    showImages('.freehand')
+});
